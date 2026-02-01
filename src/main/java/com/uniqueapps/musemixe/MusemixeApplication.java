c243cb82b83f8174f336f72e06ea89386b7a9155
Arpan Chatterjee <arpanchatterjee1647@gmail.com>
2026-02-01 12:55:46 +0530

    final fixes

--- src/main/java/com/uniqueapps/musemixe/MusemixeApplication.java
+++ src/main/java/com/uniqueapps/musemixe/MusemixeApplication.java
@@ -9,18 +9,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class MusicApplication extends Application {
+public class MusemixeApplication extends Application {
 
     private HomeController homeController;
 
     @Override
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
-        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(MusicApplication.class.getResource("home.fxml")));
+        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(MusemixeApplication.class.getResource("home.fxml")));
         Scene scene = new Scene(loader.load(), 800, 600);
-        scene.getStylesheets().add(Objects.requireNonNull(MusicApplication.class.getResource("style.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(MusemixeApplication.class.getResource("style.css")).toExternalForm());
         homeController = loader.getController();
-        stage.setTitle("MIDI Player");
+        stage.setTitle("Musemixe");
         stage.setScene(scene);
         stage.show();
     }
